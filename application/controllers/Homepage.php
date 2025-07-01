aa0d8fa2cbf661c7e4cfb393fda1f238a49acffc
Maherhakim123 <hakim05013@gmail.com>
2025-07-01 18:00:13 +0800

    echo

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -7,13 +7,13 @@ class Homepage extends CI_Controller
     public function index()
     {
         // Load view or echo something
-        // echo 'This is the homepage.';
+        echo 'This is the homepage.';
         // Or load a view: $this->load->view('homepage');
-        redirect('Homepage/Homepage');
+        // redirect('Homepage/Homepage');
     }
 
-    public function Homepage()
-    {
-        $this->load->view('Homepage');
-    }
+    // public function Homepage()
+    // {
+    //     $this->load->view('Homepage');
+    // }
 }
