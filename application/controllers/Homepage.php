4cb60379b7eafc7df59e173ac4b3ba604c432828
Maherhakim123 <hakim05013@gmail.com>
2025-07-01 16:49:46 +0800

    add new homepage controller

---
+++ application/controllers/Homepage.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Homepage extends CI_Controller
+{
+    public function index()
+    {
+        // Load view or echo something
+        echo 'This is the homepage.';
+        // Or load a view: $this->load->view('homepage');
+    }
+}
