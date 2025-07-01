64ed13a1e9a3eea2d08522ab4dbea567a707e0d5
Maherhakim123 <hakim05013@gmail.com>
2025-07-01 16:30:57 +0800

    edit routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,6 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -49,18 +50,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'homepage';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = false;
 
 $route['abouts'] = 'Welcome/demo';
-
 
 // Login and Register for user and admin
 $route['register']['GET'] = 'Auth/RegisterController/index';
 $route['register']['POST'] = 'Auth/RegisterController/register';
 
-// Create New Project and 
+// Create New Project and
 $route['project/create'] = 'project/create';
 $route['project/new'] = 'project/store';
 
@@ -73,6 +73,3 @@ $route['project/delete/(:num)'] = 'project/delete/$1'; // Delete route
 $route['auth/profile'] = 'auth/profile';
 
 $route['dashboard/dashboard'] = 'dashboard/dashboard';
-
-
-
