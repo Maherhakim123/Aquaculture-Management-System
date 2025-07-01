00a8cc773a2624aa05d1a602a97f8285fac3b857
Maherhakim123 <hakim05013@gmail.com>
2025-07-01 17:36:07 +0800

    redirect dahsboard

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -8,7 +8,7 @@ class Homepage extends CI_Controller
     {
         // Load view or echo something
         // echo 'This is the homepage.';
-        $this->load->view('Homepage');
-        // redirect('dashboard/homepage');
+        // Or load a view: $this->load->view('homepage');
+        redirect('dashboard/homepage');
     }
 }
